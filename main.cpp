a955fcfccc59c8badce88a3a11ba31493ba600d9
S1NLESS1 <xryak.uoy@gmail.com>
2024-05-22 20:06:04 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,47 +14,39 @@ int main() {
 	Level level;
 	level.LoadFromFile("Tiles/MapMain.tmx");
 
-	Game *game = new Game(&level); // ñîçäàíèå âñåõ îáúåêòîâ
-	b2World *world = game->get_world();
+	Game* game = new Game(&level); // ÑÐ¾Ð·Ð´Ð°Ð½Ð¸Ðµ Ð²ÑÐµÑ… Ð¾Ð±ÑŠÐµÐºÑ‚Ð¾Ð²
+	b2World* world = game->get_world();
 	sf::RenderWindow game_window(sf::VideoMode(1500, 1000), "Test");
-	sf::FloatRect visibleArea = sf::FloatRect(0.f, 0.f, game_window.getSize().x * 0.4, game_window.getSize().y * 0.9);// äëÿ ðåãèñòðàöèè ìàñøòàáèðîâàíèÿ îêíà
-
-	// îêíà ãåðîåâ
-	sf::View player1View;
-	player1View.setViewport(sf::FloatRect(0.f, 0.f, 0.5f, 1.f));
-	player1View.reset(visibleArea);
-	sf::View player2View;
-	player2View.setViewport(sf::FloatRect(0.5f, 0.f, 0.5f, 1.f));
-	player2View.reset(visibleArea);
-
-
-	//Öèêë èãðû
+	sf::FloatRect visibleArea = sf::FloatRect(0.f, 0.f, game_window.getSize().x * 0.4, game_window.getSize().y * 0.9);// Ð´Ð»Ñ Ñ€ÐµÐ³Ð¸ÑÑ‚Ñ€Ð°Ñ†Ð¸Ð¸ Ð¼Ð°ÑÑˆÑ‚Ð°Ð±Ð¸Ñ€Ð¾Ð²Ð°Ð½Ð¸Ñ Ð¾ÐºÐ½Ð°
+	
+	//Ð¦Ð¸ÐºÐ» Ð¸Ð³Ñ€Ñ‹
 	while (game_window.isOpen()) {
 		sf::Event event;
 		game_window.pollEvent(event);
 
-			//Äåéñòâèÿ
-			switch (event.type) {
+		//Ð”ÐµÐ¹ÑÑ‚Ð²Ð¸Ñ
+		switch (event.type) {
 
-			case sf::Event::Closed:
-				game_window.close();
-				break;
-			case sf::Event::Resized:
-				// update the view to the new size of the window
-				 visibleArea = sf::FloatRect(0.f, 0.f, event.size.width * 0.4, event.size.height * 0.9);
-				 player1View.reset(visibleArea);
-				 player1View.zoom(0.7f);
-				 player2View.reset(visibleArea);
-				 player2View.zoom(0.7f);
-				break;
-			default: break;
-			}
-		
+		case sf::Event::Closed:
+			game_window.close();
+			break;
+		case sf::Event::Resized:
+			// update the view to the new size of the window
+			visibleArea = sf::FloatRect(0.f, 0.f, event.size.width * 0.4, event.size.height * 0.9);
+			break;
+		default: break;
+		}
+
 		game->behavior(event);
-		world->Step(1.0f / 3.f, 1, 1);
+		if (abs(game->get_playerBody(0)->GetPosition().x - game->get_playerBody(1)->GetPosition().x) < 250 && abs(game->get_playerBody(0)->GetPosition().y - game->get_playerBody(1)->GetPosition().y) < 200){
+			game->game_draw(&game_window, level, visibleArea, 0,"GENERAL");
+			world->Step(1.0f / 9.f, 1, 1);
+		}
+		else {
+			game->game_draw(&game_window, level, visibleArea, 0, "SINGLE");
+			world->Step(1.0f / 7.f, 1, 1);
+		}
 
-		game->game_draw(&game_window, level, player1View, 1);
-		game->game_draw(&game_window, level, player2View, 2);
 		game_window.display();
 		game_window.clear();
 	}
